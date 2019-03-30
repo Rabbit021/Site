ffc49a881577879dc5b9fc335d9d99826b94e248
wangheng <635253084@qq.com>
2019-03-30 20:07:14 +0800

    change theme

--- Bat/install.bat
+++ Bat/install.bat
@@ -2,20 +2,26 @@
 echo 开始安装
 echo f| npm install hexo-cli -g
 echo f| npm install hexo --save
-echo f| npm install 
+
 echo f| npm install hexo-generator-index --save
 echo f| npm install hexo-generator-archive --save
 echo f| npm install hexo-generator-category --save
 echo f| npm install hexo-generator-tag --save
+echo f| npm install hexo-generator-feed@1 --save
+echo f| npm install hexo-generator-sitemap@1 --save
+
 echo f| npm install hexo-server --save
+
 echo f| npm install hexo-deployer-git --save
+
 echo f| npm install hexo-deployer-heroku --save
 echo f| npm install hexo-deployer-rsync --save
 echo f| npm install hexo-deployer-openshift --save
+
+echo f| npm install hexo-renderer-jade@0.3.0 --save
+echo f| npm install hexo-renderer-stylus --save
 echo f| npm install hexo-renderer-marked@0.2 --save
 echo f| npm install hexo-renderer-stylus@0.2 --save
-echo f| npm install hexo-generator-feed@1 --save
-echo f| npm install hexo-generator-sitemap@1 --save
-echo f|  npm install hexo-deployer-git --save
+
 echo 安装完成
 @pause
