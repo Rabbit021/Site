fd24097325b1546921c1de7188f5f9baa48d2a8f
wangheng <635253084@qq.com>
2019-03-31 17:14:24 +0800

    配置主题

--- Bat/install.bat
+++ Bat/install.bat
@@ -3,12 +3,17 @@ echo 开始安装
 echo f| npm install hexo-cli -g
 echo f| npm install hexo --save
 
+
 echo f| npm install hexo-generator-index --save
 echo f| npm install hexo-generator-archive --save
 echo f| npm install hexo-generator-category --save
 echo f| npm install hexo-generator-tag --save
-echo f| npm install hexo-generator-feed@1 --save
-echo f| npm install hexo-generator-sitemap@1 --save
+echo f| npm install hexo-generator-feed --save
+echo f| npm install hexo-generator-sitemap --save
+echo f| npm install hexo-generator-baidu-sitemap --save
+echo f| npm install hexo-generator-json-content@2.2.0 --save
+echo f| uninstall hexo-generator-index --save
+echo f| install hexo-generator-index-pin-top --save
 
 echo f| npm install hexo-server --save
 
@@ -23,5 +28,10 @@ echo f| npm install hexo-renderer-stylus --save
 echo f| npm install hexo-renderer-marked@0.2 --save
 echo f| npm install hexo-renderer-stylus@0.2 --save
 
+
+
 echo 安装完成
 @pause
+
+
+
