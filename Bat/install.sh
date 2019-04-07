b15a9381aa769a64c1ca3ac8d7be4a68c453dcc4
wangheng <635253084@qq.com>
2019-04-07 17:13:02 +0800

    添加sh脚本

---
+++ Bat/install.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+npm install hexo-cli -g
+npm install hexo --save
+
+npm install hexo-generator-index --save
+npm install hexo-generator-archive --save
+npm install hexo-generator-category --save
+npm install hexo-generator-tag --save
+npm install hexo-generator-feed --save
+npm install hexo-generator-sitemap --save
+npm install hexo-generator-baidu-sitemap --save
+npm install hexo-generator-json-content@2.2.0 --save
+npm uninstall hexo-generator-index --save
+npm install hexo-generator-index-pin-top --save
+
+npm install hexo-server --save
+npm install hexo-deployer-git --save
+
+npm install hexo-deployer-heroku --save
+npm install hexo-deployer-rsync --save
+npm install hexo-deployer-openshift --save
+
+npm install hexo-renderer-jade@0.3.0 --save
+npm install hexo-renderer-stylus --save
+npm install hexo-renderer-marked@0.2 --save
+npm install hexo-renderer-stylus@0.2 --save
+
+
+
+
